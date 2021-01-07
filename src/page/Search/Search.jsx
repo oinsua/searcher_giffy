43910a276cc5294d58c0995d13552bb58eb6fe23
¨oinsua¨ <ossany.insua1982@gmail.com>
2021-01-06 22:26:33 -0800

    SEO OK

--- src/page/Search/Search.jsx
+++ src/page/Search/Search.jsx
@@ -1,11 +1,17 @@
 import React, {useRef, useCallback, useEffect} from 'react';
 import { useParams, Redirect} from "react-router-dom";
+import {Helmet} from "react-helmet";
+import debounce_it from 'just-debounce-it';
+//Importando componentes
 import Searcher from './../../component/Searcher/Searcher';
 import {useSearchGiffy} from './../../hook/useSearchGiffy';
 import List_Gifs from './../../component/List_Gifs/List_Gifs';
 import Skeleton_List from '../../skeletons/List_Gif/Skeleton_List';
 import { useIntersection_Observer } from '../../hook/useIntersection_Observer';
-import debounce_it from 'just-debounce-it';
+
+//Importando el Favicon
+import Favicon from '../../assets/images/favicon.png'
+
  
 const Search = () => {
     /*Tomando los parametros enviados por la url */
@@ -29,7 +35,19 @@ const Search = () => {
            <Searcher intitialKeyword={keyword} initialRaiting={rating} initialLanguage={language}/>
            <h1 className="app-title"> Your last Search was "{decodeURI(keyword)}"</h1>
            {   //Si loading es true, muestra Skeleton sino el listado de Gif.
-               loading ? <Skeleton_List array={[1,2,3,4,5,6,7,8,9,10]} /> : <List_Gifs gifs={gifs} />
+               loading ? 
+                         <Skeleton_List array={[1,2,3,4,5,6,7,8,9,10]} /> 
+                       :  
+                       <>
+                          <Helmet>
+                                <title>Giffy-Search</title>
+                                <meta name="application-name" content="searcher_giffy" />
+                                <meta name="description" content="Giffy-Search"/>
+                                <meta name="google" content="notranslate"/>
+                                <link rel="icon" href={Favicon} sizes="32x32" type="image/png"></link>
+                            </Helmet>
+                          <List_Gifs gifs={gifs} />
+                       </>
            }
            {  //Si error es true, muestra un pagina 404
                error ? <Redirect to="/Error404" /> : null
