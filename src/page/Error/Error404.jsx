0a48f347e35a0b502c99da0aeaac0a5e0ec3db34
oinsua <ossany.insua1982gmail.com>
2021-01-12 17:04:38 -0500

    Performance

--- src/page/Error/Error404.jsx
+++ src/page/Error/Error404.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import React from 'react';
+import {Helmet} from "react-helmet";
+import {Link} from 'react-router-dom';
+
+//Importando el Favicon
+import Favicon from '../../assets/images/favicon.png'
 
 const Error404 = () => {
     return (
-        <div>
+        <>
+        <Helmet>
+          <title>Giffy-Search</title>
+          <meta name="application-name" content="searcher_giffy" />
+          <meta name="description" content="Giffy-Search"/>
+          <meta name="google" content="notranslate"/>
+          <link rel="icon" href={Favicon} sizes="32x32" type="image/png"></link>
+        </Helmet>
             <h1>SE HA PRODUCIDO UN ERROR 404</h1>
-        </div>
+            <Link to="/">GO TO GIFS HOME</Link>
+        </>
     )
 }
 
